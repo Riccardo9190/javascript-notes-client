217e2fb762ddadba33ed4a7c39205b61b840e8d3
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-07 18:27:43 -0300

    components

--- src/components/header_logged/index.js
+++ src/components/header_logged/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Navbar, Container, Column, Button, Dropdown } from 'rbx';
-import logoImage from '../../assets/images/logo-white.png';
+import logoImage from '../../assets/images/white-logo.png';
 import "../../styles/header.scss";
 import UsersService from '../../services/users';
 import { Navigate, Link } from "react-router-dom";
